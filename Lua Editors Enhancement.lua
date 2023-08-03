80b1c9efb0f8c1f800485006ce4a02019cdbddff
YoucefHam <youcef_0665@hotmail.fr>
2023-08-03 02:15:06 +0100

    Update Lua Editors Enhancement.lua

--- Lua Editors Enhancement.lua
+++ Lua Editors Enhancement.lua
@@ -21,9 +21,33 @@
 			* CTRL + SHIFT + C : toggle column selection mode.
 			* CTRL + SHIFT + L : toggle line selection mode.
 
+
+	MIT License
+
+	Copyright (c) 2023 YoucefHam
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 ]====] --
 -- TODO List
--- add auto backup;
+-- add auto backup
+-- add popup menu to go to memory address
+-- add clear befor execute in lua engine;
 local Disable_Extension = false
 if Disable_Extension then
 	return
